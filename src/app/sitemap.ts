fa7ae6816e48f4e0931bf4389feb0f9086d13e2b
bart tynior <btynior@gmail.com>
2023-09-16 16:55:10 +0200

    Fixed sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://media-control-agency.com",
+      url: "https://www.media-control-agency.com",
       priority: 1,
       changeFrequency: "daily",
       lastModified: new Date(),
     },
     {
-      url: "https://media-control-agency.com/contact",
+      url: "https://www.media-control-agency.com",
       priority: 1,
       changeFrequency: "daily",
       lastModified: new Date(),
     },
     {
-      url: "https://media-control-agency.com/privacy-policy",
+      url: "https://www.media-control-agency.com",
       priority: 1,
       changeFrequency: "daily",
       lastModified: new Date(),
