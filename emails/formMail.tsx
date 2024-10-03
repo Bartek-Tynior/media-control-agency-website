9c4c91a2ab5e0ce69dca8b4252e7074e0814b8de
Bart Tynior <btynior@gmail.com>
2024-10-03 10:31:24 +0200

    Redesign the formMail component

--- emails/formMail.tsx
+++ emails/formMail.tsx
@@ -26,20 +26,31 @@ export default function FormMail({
   return (
     <Html>
       <Tailwind>
-        <Body className="bg-white my-12 mx-auto font-sans">
-          <Container className="p-8 border-white/10 rounded-lg shadow-lg text-center">
-            <Heading className="text-2xl pt-4">
-              There is a new reach out from {fullName} | {email}
+        <Body className="bg-[#0f0f0f] my-12 mx-auto font-sans">
+          <Container className="p-8 bg-white border-white/10 rounded-lg shadow-lg text-center">
+            <Heading className="text-2xl pt-4 text-[#333] font-bold">
+              New Reach Out from {fullName}
             </Heading>
-            <Heading className="text-lg pt-4">Message</Heading>
+            <Text className="text-lg pt-2 text-[#666]">Email: {email}</Text>
+            <Heading className="text-lg pt-4 text-[#444] font-semibold">
+              Message
+            </Heading>
             <Text className="text-lg font-medium text-gray-700">{message}</Text>
-            <Heading className="text-lg pt-4">Contact details</Heading>
+            <Heading className="text-lg pt-4 text-[#444] font-semibold">
+              Contact Details
+            </Heading>
             <Text className="text-lg font-medium text-gray-700">
               Company: {company}
             </Text>
             <Text className="text-lg font-medium text-gray-700">
               Phone: {phone}
             </Text>
+            <a
+              href={`mailto:${email}`}
+              className="mt-6 inline-block px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+            >
+              Reply to {fullName}
+            </a>
           </Container>
         </Body>
       </Tailwind>
