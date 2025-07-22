28b9afeff79ba7759038586fc9ac62eb20a97489
Bart Tynior <btynior@gmail.com>
2025-07-22 20:01:59 +0200

    feat: Enhance logging for contact form submission process

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -5,13 +5,20 @@ import FormMail from "../../../../emails/formMail";
 export async function POST(req: Request) {
   const body = await req.json();
 
+  console.log("Received contact form submission:", body);
+
   try {
+    console.log("Sending email...");
+
+    console.log("Receiver:", process.env.SMTP_EMAIL);
+
     await sendEmail({
       to: process.env.SMTP_EMAIL || "",
       subject: "New Contact Form Submission!",
       html: render(FormMail(body)),
     });
   } catch (error: any) {
+    console.error("Error sending email:", error.message || error);
     return new Response("Internal Server Error!", { status: 500 });
   }
 
